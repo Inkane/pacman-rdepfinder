b08503cbfbe2528b5cdad1139cfaf42072ba5434
Fabian Kosmale <0inkane@googlemail.com>
2012-07-20 22:08:59 +0200

    using an object now

--- rdepends.py
+++ rdepends.py
@@ -2,12 +2,9 @@
 from __future__ import print_function
 import os
 import sys
+#import threading
+import collections
 import termcolor
-
-# we use global variables here, because a class is overkill in this case
-# and we don't use multithreading
-already_visited_packages = set()
-all_rdepends = set()
 
 
 def pprint_list(l):
@@ -22,26 +19,39 @@ def pprint_list(l):
     print()
 
 
-def list_rdepends(package, recurse_depth=0):
-    """print all the reverse depends of a package"""
-    if package in already_visited_packages:
-        return
-    else:
-        already_visited_packages.add(package)
-    rdepends = os.popen("""LANGUAGE=en_US pacman -Sii {0} | grep -im"""
-            """ 1 "required by" | sed -r 's/^.+://'""".format(package)).read().strip()
-    if rdepends == "None":
-        return
-    rdepends = rdepends.split()
-    print(termcolor.colored(">> ", color="blue"), "rdepends of {} are: ".format(package))
-    pprint_list(rdepends)
-    if recurse_depth:
-        for pac in rdepends:
-            all_rdepends.add(pac)
-            list_rdepends(pac, recurse_depth - 1)
+class RdependsFinder(object):
+
+    def __init__(self, recur_depth=5):
+        self.already_visited_packages = set()
+        self.all_rdepends = set()
+        self.pkg2rdep = collections.OrderedDict()
+        self.recur_depth = recur_depth
+
+    def list_rdepends(self, package, recur=None):
+        """print all the reverse depends of a package"""
+        if recur == None:
+            recur = self.recur_depth
+        if package in self.already_visited_packages:
+            return
+        else:
+            self.already_visited_packages.add(package)
+        rdepends = os.popen("""LANGUAGE=en_US pacman -Sii {0} | grep -im"""
+                """ 1 "required by" | sed -r 's/^.+://'""".format(package)).read().strip()
+        if rdepends == "None":
+            return
+        rdepends = rdepends.split()
+        self.pkg2rdep[package] = rdepends
+        if self.recur_depth:
+            for pac in rdepends:
+                self.all_rdepends.add(pac)
+                self.list_rdepends(pac, recur - 1)
 
 if __name__ == "__main__":
-    list_rdepends(sys.argv[1], 5)
+    rfinder = RdependsFinder(1)
+    rfinder.list_rdepends(sys.argv[1])
+    for k in rfinder.pkg2rdep:
+        print(termcolor.colored(">> ", color="blue"), "rdepends of {} are: ".format(k))
+        pprint_list(rfinder.pkg2rdep[k])
     print()
     print(termcolor.colored(">> ", color="blue"), "All rdepends:")
-    pprint_list(all_rdepends)
+    pprint_list(rfinder.all_rdepends)
