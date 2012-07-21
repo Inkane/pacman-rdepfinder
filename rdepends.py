547d75b2703203704b4cba369fa7997d063aa50e
Fabian Kosmale <0inkane@googlemail.com>
2012-07-21 12:39:09 +0200

    finer grained locks

--- rdepends.py
+++ rdepends.py
@@ -9,7 +9,7 @@ import argparse
 import logging
 from time import sleep
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def pprint_list(l):
@@ -27,7 +27,6 @@ def pprint_list(l):
 class RdependsFinder:
 
     lock = threading.Lock()
-    #lock2 = threading.BoundedSemaphore()
 
     def __init__(self, recur_depth=1, max_workers=4):
         self._already_visited_packages = set()
@@ -64,18 +63,20 @@ class RdependsFinder:
             return
         else:
             self._already_visited_packages.add(package)
-        logging.debug("after already_visited_packages")
-        #RdependsFinder.lock.release()
+        logging.debug("releasing...")
+        RdependsFinder.lock.release()
+        logging.debug("released!")
         rdepends = os.popen("""LANGUAGE=en_US pacman -Sii {0} | grep -im"""
                 """ 1 "required by" | sed -r 's/^.+://'""".format(package)).read().strip()
-        logging.debug("after os.popen")
         if rdepends == "None":
-            logging.debug("releasing...")
-            RdependsFinder.lock.release()
-            logging.debug("released!")
+            #logging.debug("releasing...")
+            #RdependsFinder.lock.release()
+            #logging.debug("released!")
             return
         rdepends = rdepends.split()
-        #RdependsFinder.lock.acquire()
+        logging.debug("locking...")
+        RdependsFinder.lock.acquire()
+        logging.debug("locked!")
         self._all_rdepends = self._all_rdepends.union(set(rdepends))
         self._pkg2rdep[package] = rdepends
         logging.debug("releasing...")
